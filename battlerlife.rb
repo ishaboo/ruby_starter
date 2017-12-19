722a04e013cd95a22bdb4167e865ee19bdda02f1
Shnaps <fartsopup@gmail.com>
2017-12-18 19:46:18 -0500

    initial commit

---
+++ battlerlife.rb
@@ -0,0 +1,38 @@
+stopp = true
+puts "Welcome (you Humans)!"
+puts "whats your Name?" 
+your_answer = puts your_answer 
+your_inventory = {:Apples => 5, :Bucks => 222}
+puts your_inventory
+puts "You give"
+puts 3 + 7
+puts "Bucks and"
+puts 4 + 4
+puts "coins to the market man and say bye."
+stopp = gets.chomp
+ans = gets.chomp
+while stopp == true
+if stopp == stop
+  puts "bye"
+  stopp = false
+if ans == 'inv'
+	puts "You have #{your_inventory}"
+while stopp == true	
+  if ans == 'craft'
+  	print "What do you want to craft?"
+  if ans == 'mashed apples'
+  if your_inventory[:Apples] > 3
+  	puts "You have more than 3 apples!" 
+  if ans == 'craft mashed Apples'
+  	puts "You have #{your_inventory} and mashed Apples !"
+  if ans == 'position'
+     puts "in the BATTLEHALL1 !!! ps: its better if you go North => n"
+                end
+   				    end
+   			    end
+   			  end
+   		  end
+ 	    end
+ 	  end 	  
+  end
+end
