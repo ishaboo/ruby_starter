ee2ae986199dea949878287c0e3248d3ee0f134d
Shnaps <fartsopup@gmail.com>
2017-12-20 13:56:31 -0500

    need help

--- Battler_life.rb.rb
+++ Battler_life.rb.rb
@@ -1,25 +1,31 @@
-def Greeting
-	puts "Welcome to Battler_life."
-	puts "What is your Name?"
-	your_name = gets.chomp
-	puts your_name
-end
-while answer == 'n'
-def your_spell
-	puts "What is your_Spell?"
-	spell = gets.chomp
-	puts spell "are you shure ? y/n"
+puts "Welcome to Battler_life."
+puts "What is your Name?"
+your_name = gets.chomp
+puts your_name
+answer = 'no'
+while answer == 'no'
+	puts "What is your_spell?"
+	yourspell = gets.chomp
+	print yourspell + " are you shure ? yes/no"
 	answer = gets.chomp
-	if answer == 'y'
-		puts "okay so  #{your_Spell} is your spell."
+	if answer == 'yes'
+		puts "okay so  #{yourspell} is your spell."
 	else
-		spell = gets.chomp
+		your_spell = gets.chomp
 		puts "Enter your spell"
-end
-your_inventory = {:bronze_sword => 1, :bucks =>56}
+	end
+your_inventory = {:bronze_sword => 1, :bucks => 56}
 puts your_inventory
+answer = gets.chomp
+if answer == 'inv'
+		puts your_inventory
 end
-def equipment
-if answer == inv
-	puts your_inventory
+puts "do you want to play? yes/no"
+answer = gets.chomp
+while answer == 'yes'
+	if answer == 'yes'
+	else 
+		puts "====>bye!"
+	end
+end
 end
