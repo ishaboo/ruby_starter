978aae56e0e68e509102eef5667593d60aa0da74
Shnaps <fartsopup@gmail.com>
2017-12-20 13:00:05 -0500

    need help

---
+++ Battler_life.rb.rb
@@ -0,0 +1,25 @@
+def Greeting
+	puts "Welcome to Battler_life."
+	puts "What is your Name?"
+	your_name = gets.chomp
+	puts your_name
+end
+while answer == 'n'
+def your_spell
+	puts "What is your_Spell?"
+	spell = gets.chomp
+	puts spell "are you shure ? y/n"
+	answer = gets.chomp
+	if answer == 'y'
+		puts "okay so  #{your_Spell} is your spell."
+	else
+		spell = gets.chomp
+		puts "Enter your spell"
+end
+your_inventory = {:bronze_sword => 1, :bucks =>56}
+puts your_inventory
+end
+def equipment
+if answer == inv
+	puts your_inventory
+end
--- battle_life.rb
+++ battle_life.rb
@@ -10,19 +10,28 @@ end
 
 $inventory = {
   "Apples" => 3,
-  "Bucks" => 222
+  "Bucks" => 222,
+  "Satchel" => []
 }
 
 name = greeting
 puts "Hello #{name}, your inventory is a hash, it looks like this: \n #{$inventory}"
 
-puts "How much do you want to spend?"
+# puts "How much do you want to spend?"
 spend = gets.chomp
 
 def spend_money(money)
   $inventory["Bucks"] -= money.to_i
 end
 
-spend_money(spend)
+def put_into_satchel
+	puts "What do you want to put into your satchel?"
+	stuff = gets.chomp
+	$inventory["Satchel"] << stuff
+end
+
+# spend_money(spend)
+
+put_into_satchel
 
 puts "Now your inventory is: #{$inventory}"
