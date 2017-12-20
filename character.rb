c56686a3fcb100c09d9feb2e1dc144c55b3cc799
Pasquale Kress <wabisabisf@gmail.com>
2017-12-20 23:15:00 +0100

    load this in irb and try it

--- character.rb
+++ character.rb
@@ -11,5 +11,20 @@ class Inventory
 end
 
 class Character
+  def initialize(attributes={})
+    @name = attributes[:name]
+    @race = attributes[:race]
+  end
 
+  def change_name(name)
+    @name = name
+  end
+
+  def change_race(race)
+    @race = race
+  end
 end
+
+
+# notes:
+# https://stackoverflow.com/questions/4554325/what-is-the-most-efficient-way-to-initialize-a-class-in-ruby-with-different-para#4554397
