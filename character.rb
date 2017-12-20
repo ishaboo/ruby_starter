62603af2956e4f003dd4666b76e39b6ead8d68bf
Pasquale Kress <wabisabisf@gmail.com>
2017-12-20 22:36:12 +0100

    load this in irb and try it

---
+++ character.rb
@@ -0,0 +1,15 @@
+class Inventory
+  def initialize(weapons, items, money)
+    @weapons = weapons
+    @items = items
+    @money = money
+  end
+
+  def add_money(amount)
+    @money += amount
+  end
+end
+
+class Character
+
+end
