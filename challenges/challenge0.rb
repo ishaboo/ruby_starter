885fac056fdcb2a2c2f3b1ec6a6092a9d20cb08c
Pasquale Kress <wabisabisf@gmail.com>
2018-01-01 19:36:13 +0100

    good job

--- challenges/challenge0.rb
+++ challenges/challenge0.rb
@@ -1,13 +1,19 @@
 puts "Welcome, whats your Name?"
 name = gets.chomp
 puts name
-array = ["Apple", 3, "Gold", 4, "map", 1, "flyer", 4,]
+array = ["Apple", 3, "Gold", 4, "   map   ", 1, "flyer", 4,]
+array2 = [1,2,44,5,2,323,5,6,7,8]
 puts "type: Apple or type: Gold"
 ans = gets.chomp
-if array.include? == ans
+if array.include?(ans)
+  puts "reversed:"
 	puts array.reverse
-	puts array.sort
-	puts array.strip
+  puts "sorted:"
+	puts array2.sort
+  puts "    map    stripped:"
+	puts array[4].strip
+  puts "random value:"
 	puts array.sample
+  puts "just the second to fourth element:"
 	puts array[1..3]
 end
