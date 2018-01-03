d05952f48d7062064e7c07aa69644ca4240df54e
Pasquale Kress <wabisabisf@gmail.com>
2018-01-03 23:25:27 +0100

    sample Map class

--- mud/ideas/Character.rb
+++ mud/ideas/Character.rb
@@ -16,3 +16,27 @@ class Character
     @coords[:y] += y
   end
 end
+
+
+class Map
+  attr_reader :info, :coords
+  attr_writer :info
+
+  def initialize(attributes={})
+    @coords = attributes[:coords]
+    @info = attributes[:info]
+  end
+
+  def locate_character(coords)
+    if coords[:x] && coords[:y]
+      puts @info
+    end
+  end
+end
+
+
+# Seed?
+a = Map.new({:coords => {:x=>3, :y=>5}})
+b = Character.new
+a.info = "Success"
+a.locate_character(b.coords)
