c2cdde319338b6ed1979dbda3b850ce5727c9a03
Pasquale Kress <wabisabisf@gmail.com>
2018-01-04 22:49:44 +0100

    just an idea for a CSV based mock DB

--- mud/ideas/Character.rb
+++ mud/ideas/Character.rb
@@ -36,6 +36,49 @@ class Map
   end
 end
 
+require 'csv'
+# require_relative 'map'
+
+class MapTile
+  attr_reader :map_tiles
+
+  def initialize(csv_file)
+    # where to store my values
+    # @map_tiles = []
+    @csv_file = csv_file
+    # Brain twister ****
+    @map_tiles = CSV.read(@csv_file).map do |tile|
+      Map.new({tile[0], tile[1]})
+    end
+  end
+
+  def add_tile(new_tile)
+    # This adds a new tile to the map
+    @map_tiles << new_tile
+    write_to_csv(@map_tiles)
+  end
+
+  def remove_recipe(tile_id)
+    # this removes a tile from the map.
+    @map_tiles.delete_at(tile_id)
+    write_to_csv(@map_tiles)
+  end
+
+  def all
+    @map_tiles
+  end
+
+  def write_to_csv(map_tiles)
+    CSV.open(@csv_file, "wb") do |csv|
+      map_tiles.each do |tile|
+        # Here, row is an array of columns
+        csv << [tile.name, tile.description]
+      end
+    end
+  end
+end
+
+
 
 # Seed?
 a = Map.new({:coords => {:x=>3, :y=>5}})
