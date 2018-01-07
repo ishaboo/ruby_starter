2034cbe27c55317c1c2e33b4459897a63177b491
Pasquale Kress <wabisabisf@gmail.com>
2018-01-07 01:01:51 +0100

    added idea view class

--- mud/ideas/character.rb
+++ mud/ideas/character.rb
@@ -92,6 +92,14 @@ class MapTile
   end
 end
 
+class View
+  def show_map(map_tile)
+    # map_tile is an array
+    map_tile.each do |tile|
+      puts "#{tile[1]}"
+    end
+  end
+end
 
 
 # Seed?
