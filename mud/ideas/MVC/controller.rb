9748c1918a3f1d29e9699d7e8d7a780fcedc2ba9
Pasquale Kress <wabisabisf@gmail.com>
2018-01-08 23:33:39 +0100

    getting started on the controller

---
+++ mud/ideas/MVC/controller.rb
@@ -0,0 +1,19 @@
+require_relative 'character'
+require_relative 'map'
+require_relative 'maptile'
+require_relative 'view'
+
+class Controller
+  def initialize(map)
+    @map = map
+    @view = View.new
+  end
+
+  def list
+
+  end
+
+  def create
+
+  end
+end
