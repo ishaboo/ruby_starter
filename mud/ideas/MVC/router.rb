12c195bf2c3f0738c5eb2c95675034c594a7c803
Pasquale Kress <wabisabisf@gmail.com>
2018-01-10 22:30:41 +0100

    added some routing

--- mud/ideas/MVC/router.rb
+++ mud/ideas/MVC/router.rb
@@ -1,3 +1,5 @@
+require_relative 'controller'
+
 class Router
   def initialize(controller)
     @controller = controller
@@ -9,10 +11,32 @@ class Router
 
     while @running
       display_tasks
+      action = gets.chomp
+      route(action)
     end
+  end
+
+  private
+
+  def route(action)
+    case
+    when action == 'n' then puts "north"
+    when action == 's' then puts "south"
+    when action == 'e' then puts "east"
+    when action == 'w' then puts "west"
+    when action == 'look'
+      puts "You look around..."
+    when action == 'stop' then stop
+    else
+      puts "Please enter a valid command"
+  end
+
+  def stop
+    @running = false
   end
 
   def display_tasks
     puts "buncha stuff..."
+    puts "Which direction are you going?"
   end
 end
