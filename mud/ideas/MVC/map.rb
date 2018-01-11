fc3b986516b048517965b75e1bade19fb8289a2d
Pasquale Kress <wabisabisf@gmail.com>
2018-01-11 23:23:46 +0100

    gotta move the locate char method to controller

--- mud/ideas/MVC/map.rb
+++ mud/ideas/MVC/map.rb
@@ -1,9 +1,20 @@
+require 'csv'
+require_relative 'maptile'
+
 class Map
-  attr_reader :info, :coords
+  attr_reader :maptiles
   attr_writer :info
 
-  def initialize(attributes={})
-    @map_tiles = attributes[:map_csv]
+  def initialize(csv_file)
+    @csv_file = csv_file
+    @map_tiles = CSV.read(@csv_file).map do |map|
+      MapTile.new(map[0], map[1])
+    end
+  end
+
+  def add_tile(new_tile)
+    @map_tiles << new_tile
+    write_to_csv(@map_tiles)
   end
 
   def locate_character(coords)
@@ -12,5 +23,14 @@ class Map
     # if coords[:x] && coords[:y]
     #   puts @info
     # end
+  end
+
+  def write_to_csv(map_tiles)
+    CSV.open(@csv_file, "wb") do |csv|
+      map_tiles.each do |tile|
+        # Here, row is an array of columns
+        csv << tile[0] + tile[1]
+      end
+    end
   end
 end
