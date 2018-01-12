e8fe21c8199e0b23e2ec3ff87d0828ffdd7175a5
Pasquale Kress <wabisabisf@gmail.com>
2018-01-12 21:59:56 +0100

    works fine

--- mud/ideas/MVC/view.rb
+++ mud/ideas/MVC/view.rb
@@ -3,4 +3,8 @@ class View
     # map_tile_info could be an array
     puts map_tile_info
   end
+
+  def view_stats(char_stats)
+    # just an idea to display some info about the character
+  end
 end
