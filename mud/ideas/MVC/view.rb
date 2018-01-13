617c2e602f1a1eca38658cfb4efdaa29f87d5d0a
Pasquale Kress <wabisabisf@gmail.com>
2018-01-13 23:33:46 +0100

    moved the beginning intro text into the view

--- mud/ideas/MVC/view.rb
+++ mud/ideas/MVC/view.rb
@@ -1,4 +1,14 @@
 class View
+  def start_game
+    puts `clear`
+    puts " Welcome to this MUD\n Type 'look' to see where you are on the map: "
+    puts "The following commands will also work:"
+    puts "stats to see your character info"
+    puts "save to save your location for next time"
+    puts "n for north, s for south and so on"
+    puts "stop to stop the program"
+  end
+
   def show_map(map_tile_info)
     # map_tile_info could be an array
     puts map_tile_info
