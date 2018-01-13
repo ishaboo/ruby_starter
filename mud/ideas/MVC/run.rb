93c771ec5b01a9bf94747bfeea6cca76f7e694e2
Pasquale Kress <wabisabisf@gmail.com>
2018-01-13 23:32:21 +0100

    loading the character csv file now

--- mud/ideas/MVC/run.rb
+++ mud/ideas/MVC/run.rb
@@ -2,9 +2,12 @@ require_relative 'map'
 require_relative 'controller'
 require_relative 'router'
 
-csv_file = File.join(__dir__, 'maptiles.csv')
-map = Map.new(csv_file)
-controller = Controller.new(map)
+csv_map = File.join(__dir__, 'maptiles.csv')
+# In the future there needs to be more than one char in here
+csv_character = File.join(__dir__, 'character.csv')
+map = Map.new(csv_map)
+character = Character.new(csv_character)
+controller = Controller.new(map, character)
 
 router = Router.new(controller)
 
