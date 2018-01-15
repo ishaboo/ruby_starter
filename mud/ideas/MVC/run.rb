ddb20c4e1b8e2c73aa30ba0662c41952d8828d65
Pasquale Kress <wabisabisf@gmail.com>
2018-01-15 23:10:46 +0100

    initialized an example inventory

--- mud/ideas/MVC/run.rb
+++ mud/ideas/MVC/run.rb
@@ -5,8 +5,10 @@ require_relative 'router'
 csv_map = File.join(__dir__, 'maptiles.csv')
 # In the future there needs to be more than one char in here
 csv_character = File.join(__dir__, 'character.csv')
+csv_inventory = File.join(__dir__, 'items.csv')
 map = Map.new(csv_map)
-character = Character.new(csv_character)
+inventory = Inventory.new(csv_inventory)
+character = Character.new(csv_character, inventory)
 controller = Controller.new(map, character)
 
 router = Router.new(controller)
