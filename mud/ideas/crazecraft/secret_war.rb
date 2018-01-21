5860228926b8e0b7963d3c424f62636b249a7fa1
Shnaps <fartsopup@gmail.com>
2018-01-21 13:19:01 -0500

    looks okay with wolli world

--- mud/ideas/crazecraft/secret_war.rb
+++ mud/ideas/crazecraft/secret_war.rb
@@ -1,11 +1,10 @@
-puts "type: create secret_war"
-def secret_war
+puts "Welcome, to secret_war!"
+ans = gets.chomp
+if ans == 'create secret_war' or ans == 'war'
+	puts "How many People are in your team? [1..800]"
+while ans != [1..800]
+	puts "That is no valid Number"
 	ans = gets.chomp
-	if ans == 'create secret_war'
-		puts "How many People are in your team?"
-	until ans == [1..500]
-		puts "that is no Number"
-		ans = gets.chomp
-	end
-	end
+end
+puts "Aha, Intresting...#{ans}"
 end
