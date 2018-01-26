54a5ff24ae352d1114be28107656471f798be801
Pasquale Kress <wabisabisf@gmail.com>
2018-01-26 23:46:04 +0100

    added create view and load

--- mud/ideas/sqliteMVC/app/controllers/characters_controller.rb
+++ mud/ideas/sqliteMVC/app/controllers/characters_controller.rb
@@ -1,5 +1,25 @@
+require_relative '../models/character'
+require_relative '../views/character_view'
+
 class CharactersController
   def initialize
+    @view = CharactersView.new
+  end
 
+  def create
+    # Make a new Character
+    name = @view.new_char("name")
+    race = @view.new_char("race")
+    character = Character.new(name: name, race: race)
+    character.save
+  end
+
+  def view_char
+    @view.view_char(@character)
+  end
+
+  def load_char
+    char_id = @view.find_char
+    @character = Character.find(char_id)
   end
 end
