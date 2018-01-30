e248f833c6d4392c784a35e5e9f1a817d28cafe6
Pasquale Kress <wabisabisf@gmail.com>
2018-01-30 22:36:45 +0100

    added some basic views

--- mud/ideas/active_record/app/views/characters_view.rb
+++ mud/ideas/active_record/app/views/characters_view.rb
@@ -1,13 +1,20 @@
 class CharactersView
-  def list_all
+  def start_game
+    banner = Banner.new
+    puts banner.greeting
+  end
 
+  def display_tasks
+    puts "Welcome"
+    puts "PLease enter 'new' to create a new Character"
   end
 
   def ask_for(word)
-
+    puts "Please enter #{word}:"
+    input = gets.chomp
   end
 
   def give_msg(message)
-
+    puts message
   end
 end
