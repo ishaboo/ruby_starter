c0c535507831caa26c149abb2d05e45985478e4d
Pasquale Kress <wabisabisf@gmail.com>
2018-02-02 22:49:25 +0100

    just a quick inventory seed

--- mud/ideas/active_record/app/controllers/inventory_controller.rb
+++ mud/ideas/active_record/app/controllers/inventory_controller.rb
@@ -1,6 +1,21 @@
 class InventoryController
   def initialize
+    setup
+  end
 
+  def setup
+    puts "We need to create some inventory items to test with... "
+    inventory = InventoryItem.new
+    puts "Enter an item name: "
+    name = gets.chomp
+    inventory.name = name
+    puts "Enter an amount:"
+    amount = gets.chomp
+    inventory.amount = amount
+    puts "Which Character does this item belong to?"
+    id = gets.chomp
+    inventory.character = Character.find(id.to_i)
+    inventory.save
   end
 
   def show_inventory
