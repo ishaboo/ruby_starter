80096371a0587c92c43858464ba364850d000108
Pasquale Kress <wabisabisf@gmail.com>
2018-02-04 02:57:25 +0100

    added maptiles to the DB

---
+++ mud/ideas/active_record/db/migrate/20180204004524_create_map_tiles.rb
@@ -0,0 +1,12 @@
+class CreateMapTiles < ActiveRecord::Migration[5.0]
+  def change
+    create_table :map_tiles do |t|
+      t.string :name
+      t.string :description
+      t.string :items
+      t.integer :x_coord
+      t.integer :y_coord
+      t.timestamps
+    end
+  end
+end
