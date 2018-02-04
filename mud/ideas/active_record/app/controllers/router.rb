4fae62584d45e667e82c9e063aa2e03cab3436b2
Pasquale Kress <wabisabisf@gmail.com>
2018-02-04 22:26:57 +0100

    added routes to move and show char

--- mud/ideas/active_record/app/controllers/router.rb
+++ mud/ideas/active_record/app/controllers/router.rb
@@ -22,12 +22,13 @@ class Router
 
   def route(action)
     case
-    when action.length == 1 then @char_controller.move_char(action)
+    when action.length == 1 then @char_controller.move_char(@character, action)
     when action == 'new' then @char_controller.create
     when action == 'load' then @character = @char_controller.load
     when action == 'delete' then @char_controller.destroy
     when action == 'inv' then @inv_controller.show_inventory(@character)
     when action == 'look' then @map_controller.read_title(@character.x_coord, @character.y_coord)
+    when action == 'pos' then @char_controller.show_pos(@character)
     when action == 'stop' || action == 'exit' then stop
     else
       puts "Please enter a valid command"
