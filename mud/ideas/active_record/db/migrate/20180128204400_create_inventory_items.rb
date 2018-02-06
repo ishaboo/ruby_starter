30c751ed8612df5faa1a4145242fff522f9ef095
Pasquale Kress <wabisabisf@gmail.com>
2018-02-07 00:33:05 +0100

    added foreign key to items

--- mud/ideas/active_record/db/migrate/20180128204400_create_inventory_items.rb
+++ mud/ideas/active_record/db/migrate/20180128204400_create_inventory_items.rb
@@ -6,6 +6,7 @@ class CreateInventoryItems < ActiveRecord::Migration[5.0]
       t.timestamps
 
       t.references  :character, index: true, foreign_key: true
+      t.references  :map_tile, index: true, foreign_key: true
     end
   end
 end
