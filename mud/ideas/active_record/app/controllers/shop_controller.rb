34da3bb65c47ec8857e36591343a2ac61a13091b
Pasquale Kress <wabisabisf@gmail.com>
2018-02-09 23:25:07 +0100

    added method to list items in a shop

--- mud/ideas/active_record/app/controllers/shop_controller.rb
+++ mud/ideas/active_record/app/controllers/shop_controller.rb
@@ -1,8 +1,24 @@
+# require "pry-byebug"
+
 class ShopController
   def check(char)
     tile = find_tile(char)
     if tile.shop
       puts "#{tile.shop.description}"
+    else
+      puts "There is no shop here..."
+    end
+  end
+
+  def list_items(char)
+    tile = find_tile(char)
+     if tile.shop
+      puts "This shop has the following items:"
+      if tile.shop.inventory_items
+        tile.shop.inventory_items.each do |item|
+          puts "#{item.name} price: #{item.value} coins"
+        end
+      end
     else
       puts "There is no shop here..."
     end
