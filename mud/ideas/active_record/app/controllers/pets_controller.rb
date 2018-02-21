63a3615bcec9ecc335266b5baf9760ce98c98e49
Pasquale Kress <wabisabisf@gmail.com>
2018-02-21 21:28:21 +0100

    generic method to add motion to pets

--- mud/ideas/active_record/app/controllers/pets_controller.rb
+++ mud/ideas/active_record/app/controllers/pets_controller.rb
@@ -1,5 +1,24 @@
 class PetController
-  def motion
+  def initialize
+    # Let's try this with one single Pet
+    @pet = Pet.first
+  end
+
+  def moves
     # some random motion to make pet move around
+    runs = (1..4).to_a.sample
+    offset = (1..2).to_a.sample
+    if runs.even?
+      @pet.y_coord += runs
+      @pet.x_coord += offset
+    else
+      @pet.y_coord -= offset
+      @pet.x_coord -= runs
+    end
+    @pet.save
+  end
+
+  def returns_home
+    @pet.update_attributes(y_coord: 0, x_coord: 0)
   end
 end
