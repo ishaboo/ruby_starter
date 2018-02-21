c10819f6881448ef036e4398666dfa7b92377810
Pasquale Kress <wabisabisf@gmail.com>
2018-02-21 21:30:17 +0100

    fixed column name

--- mud/ideas/active_record/db/migrate/20180220213011_create_pets.rb
+++ mud/ideas/active_record/db/migrate/20180220213011_create_pets.rb
@@ -2,7 +2,7 @@ class CreatePets < ActiveRecord::Migration[5.0]
   def change
     create_table :pets do |t|
       t.string :name
-      t.string :type
+      t.string :kind
       t.integer :x_coord
       t.integer :y_coord
       t.timestamps
