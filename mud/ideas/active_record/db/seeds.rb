a765585912b35eaf3ad42009c8ca193d2f66e66c
Pasquale Kress <wabisabisf@gmail.com>
2018-03-04 21:26:17 +0100

    added some bots and hps plus strength to characters

--- mud/ideas/active_record/db/seeds.rb
+++ mud/ideas/active_record/db/seeds.rb
@@ -1,12 +1,12 @@
-a = Character.new(name: "Mygdin", race: "Elf", x_coord: 2, y_coord: 2)
+a = Character.new(name: "Mygdin", race: "Elf", hitpoints: 28, strength: 7, x_coord: 2, y_coord: 2)
 a.save
-b = Character.new(name: "Govin", race: "Elf", x_coord: 0, y_coord: 0)
+b = Character.new(name: "Govin", race: "Elf", hitpoints: 26, strength: 6, x_coord: 0, y_coord: 0)
 b.save
-c = Character.new(name: "Donder", race: "Human", x_coord: 0, y_coord: 0)
+c = Character.new(name: "Donder", race: "Human", hitpoints: 22, strength: 9, x_coord: 0, y_coord: 0)
 c.save
-d = Character.new(name: "Glimli", race: "Dwarf", x_coord: 0, y_coord: 0)
+d = Character.new(name: "Glimli", race: "Dwarf", hitpoints: 30, strength: 11, x_coord: 0, y_coord: 0)
 d.save
-e = Character.new(name: "Ormir", race: "Orc", x_coord: 0, y_coord: 0)
+e = Character.new(name: "Ormir", race: "Orc", hitpoints: 32, strength: 12, x_coord: 0, y_coord: 0)
 e.save
 
 one = InventoryItem.new(name: "Dagger", amount: 1)
@@ -29,6 +29,8 @@ six.character = Character.find(5)
 six.save
 
 Pet.create(name: "Smitty", kind: "Racoon", description: "A shaggy looking rodent", x_coord: 0, y_coord: 0)
+
+Bot.create(name: "Zorb", race: "Goblin", hitpoints: 24, strength: 8, x_coord: 0, y_coord: 1)
 
 x = MapTile.new(name: "Town Center", description: "The busy town's center", x_coord: 0, y_coord: 0)
 x.save
