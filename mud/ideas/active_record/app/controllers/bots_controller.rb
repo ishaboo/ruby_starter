ed1ca7f915470dedae02010c3879a5214c16dc5c
Pasquale Kress <wabisabisf@gmail.com>
2018-03-07 20:47:42 +0100

    fight only when bot is on the same maptile

--- mud/ideas/active_record/app/controllers/bots_controller.rb
+++ mud/ideas/active_record/app/controllers/bots_controller.rb
@@ -8,24 +8,26 @@ class BotsController
   end
 
   def fight(char, bot)
+    if find_char(char, bot)
     # binding.pry
     # A fight between to nearly equally strong characters
     # should last around 4 rounds ...
-    while char.hitpoints > 0 && bot.hitpoints > 0
-      damage = rand(char.strength)
-      counter = rand(bot.strength)
-      puts "#{char.name} hits #{bot.name} for
-      #{damage} hitpoints"
-      bot.update(hitpoints: bot.hitpoints - damage)
-      if bot.hitpoints <= 0
-        puts "#{bot.name} has died."
-      end
-      unless bot.hitpoints < 0
-        puts "#{bot.name} hits #{char.name} for
-        #{counter} hitpoints"
-        char.update(hitpoints: char.hitpoints - counter)
-        if char.hitpoints <= 0
-          puts "#{char.name} has died."
+      while char.hitpoints > 0 && bot.hitpoints > 0
+        damage = rand(char.strength)
+        counter = rand(bot.strength)
+        puts "#{char.name} hits #{bot.name} for
+        #{damage} hitpoints"
+        bot.update(hitpoints: bot.hitpoints - damage)
+        if bot.hitpoints <= 0
+          puts "#{bot.name} has died."
+        end
+        unless bot.hitpoints < 0
+          puts "#{bot.name} hits #{char.name} for
+          #{counter} hitpoints"
+          char.update(hitpoints: char.hitpoints - counter)
+          if char.hitpoints <= 0
+            puts "#{char.name} has died."
+          end
         end
       end
     end
