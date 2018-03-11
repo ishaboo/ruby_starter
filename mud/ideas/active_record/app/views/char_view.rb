f502a0336808b12b042fb73675b97cacdfe7d536
Pasquale Kress <wabisabisf@gmail.com>
2018-03-11 21:21:50 +0100

    cleaned help views just as mentioned in readme notes

---
+++ mud/ideas/active_record/app/views/char_view.rb
@@ -0,0 +1,29 @@
+class CharView
+  def ask_for(word)
+    puts "Please enter #{word}:"
+    input = gets.chomp
+  end
+
+  def give_msg(message)
+    puts message
+  end
+
+  def list_all(list)
+    list.each do |char|
+      puts "#{char.id}: #{char.name}, #{char.race}"
+    end
+  end
+
+  def view
+    puts "Your character can do the following things:\n
+    list    --> gives you a list of all the available characters,\n
+    new     --> will create a character,\n
+    update  --> will edit an existing character,\n
+    load    --> will load a character,\n
+    save    --> will save your current position and inventory,\n
+    delete  --> will delete an existing character,\n
+    inv     --> will show you the current character's inventory,\n
+    drop    --> will drop an inventory item,\n
+    grab    --> will let you pick up an item"
+  end
+end
