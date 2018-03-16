06f6a12548594db4b15e4693debcb022c75c45cc
Pasquale Kress <wabisabisf@gmail.com>
2018-03-16 21:59:42 +0100

    drawn map in ascii

---
+++ mud/ideas/active_record/app/views/ascii_map.rb
@@ -0,0 +1,36 @@
+class Map
+  def draw_map
+    puts "
+                               Fun
+                              Store
+                                ||
+                                ||
+                                ||
+                    Bar and  General  Book
+                     Tavern   Store   Store
+                       ||       ||     ||
+                       ||       ||     ||
+                       ||       ||     ||
+            ||         ||       ||     ||
+            ||         ||       ||     ||
+            ||         ||       ||     ||
+            ||         ||       ||     ||
+            ||     ____||_______||_____||
+        Town Center____  _______  _____
+            ||         ||       ||     ||
+                       ||       ||     ||
+                       ||       ||     ||
+                   Town Square  ||     ||
+                       ||       ||     ||
+                       ||       ||     ||
+                       ||       ||     ||
+                       ||       ||     ||
+                     Temple    Arena  Library
+                                       ||
+                                       ||
+                                       ||
+                                    City of
+                                  Necromancers
+    "
+  end
+end
