b9c3b4dde0bb01a03938965b36e41b9fe8632146
Pasquale Kress <wabisabisf@gmail.com>
2018-03-27 21:46:37 +0200

    help text for pets

---
+++ mud/ideas/active_record/app/views/pet_view.rb
@@ -0,0 +1,8 @@
+class PetView
+  def view
+    puts "There are animals in this game which you can interact with.\n
+    These are the commands you can use with these pets:\n
+    look more   --> this will tell you if there is a pet near you\n
+    call pet    --> if your character owns a pet, this wall it to your location"
+  end
+end
