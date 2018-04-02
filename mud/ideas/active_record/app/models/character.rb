1832f1496d2449e2afa1bc7c54207b91be143eb6
Pasquale Kress <wabisabisf@gmail.com>
2018-04-02 22:23:55 +0200

    add a method to determine special skills

--- mud/ideas/active_record/app/models/character.rb
+++ mud/ideas/active_record/app/models/character.rb
@@ -1,4 +1,10 @@
 class Character < ActiveRecord::Base
   has_many :inventory_items
   has_one :pet
+
+  disciplines = ["thief", "mage", "warrior"]
+
+  def has_discipline
+    # Internal method that determines special skills and abilities
+  end
 end
