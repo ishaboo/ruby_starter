319efc8d53102ecd4d0be9563b7d07817de0aece
Pasquale Kress <wabisabisf@gmail.com>
2018-04-03 21:55:47 +0200

    added disciplines to two characters

--- mud/ideas/active_record/db/seeds.rb
+++ mud/ideas/active_record/db/seeds.rb
@@ -1,7 +1,7 @@
-Character.create(name: "Mygdin", race: "Elf", hitpoints: 28, strength: 7, x_coord: 1, y_coord: -2, alive: true)
+Character.create(name: "Mygdin", race: "Elf", discipline: "thief", hitpoints: 28, strength: 7, x_coord: 1, y_coord: -2, alive: true)
 Character.create(name: "Govin", race: "Elf", hitpoints: 26, strength: 6, x_coord: 0, y_coord: 0, alive: true)
 Character.create(name: "Donder", race: "Human", hitpoints: 22, strength: 9, x_coord: 0, y_coord: 0, alive: true)
-Character.create(name: "Glimli", race: "Dwarf", hitpoints: 30, strength: 11, x_coord: 0, y_coord: 0, alive: true)
+Character.create(name: "Glimli", race: "Dwarf", discipline: "warrior", hitpoints: 30, strength: 11, x_coord: 0, y_coord: 0, alive: true)
 Character.create(name: "Ormir", race: "Orc", hitpoints: 32, strength: 12, x_coord: 0, y_coord: 0, alive: true)
 
 # Character's items
