90ab343ae7348b9d560455c73ff1efa440cdd77d
Pasquale Kress <wabisabisf@gmail.com>
2018-04-07 23:44:20 +0200

    added amount to weapons table

--- mud/ideas/active_record/db/migrate/20180405224508_create_weapons.rb
+++ mud/ideas/active_record/db/migrate/20180405224508_create_weapons.rb
@@ -3,6 +3,7 @@ class CreateWeapons < ActiveRecord::Migration[5.0]
     create_table :weapons do |t|
       t.string :name
       t.integer :damage
+      t.integer :amount
       t.integer :value
       t.timestamps
 
