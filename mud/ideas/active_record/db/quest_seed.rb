0d6e5a39c75236eef96e74ab810898d5412e04a0
Pasquale Kress <wabisabisf@gmail.com>
2018-06-18 22:19:44 +0200

    added some coordinates to our first quest_master

--- mud/ideas/active_record/db/quest_seed.rb
+++ mud/ideas/active_record/db/quest_seed.rb
@@ -2,6 +2,6 @@
 
 Quest.create(name: 'Test quest', length_in_minutes: 15, reward_in_exp: 220, description: 'Just a test quest', solved: false)
 
-QuestMaster.create(name: 'Laboras', race: 'Human', alive: true)
+QuestMaster.create(name: 'Laboras', race: 'Human', x_coord: 3, y_coord: -2, alive: true)
 
 Quest.first.quest_master = QuestMaster.first
