7763d359506f96d682ca48ff4f03ece89df40659
Pasquale Kress <wabisabisf@gmail.com>
2018-06-27 22:19:17 +0200

    custom seed to create monsters

---
+++ mud/ideas/active_record/db/monster_seed.rb
@@ -0,0 +1,7 @@
+Monster.create(name: "Spider", description: "A huge spider", hitpoints: 14, strength: 8, x_coord: 0, y_coord: 2, alive: true)
+Monster.create(name: "Skorpion", description: "Skorpions can sting", hitpoints: 13, strength: 10, x_coord: 2, y_coord: 1, alive: true)
+Monster.create(name: "Rat", description: "Rat's sometimes carry disease", hitpoints: 8, strength: 9, x_coord: 3, y_coord: -1, alive: true)
+Monster.create(name: "Possum", description: "A large possum", hitpoints: 12, strength: 10, x_coord: 1, y_coord: -2, alive: true)
+Monster.create(name: "Roach", description: "Roaches are quick and some fly", hitpoints: 8, strength: 6, x_coord: 3, y_coord: 1, alive: true)
+Monster.create(name: "Dog", description: "A large dangerous looking dog", hitpoints: 27, strength: 3, x_coord: 1, y_coord: -1, alive: true)
+Monster.create(name: "Mouse", description: "Don't let the small size of the mouse fool you", hitpoints: 10, strength: 11, x_coord: 3, y_coord: 0, alive: true)
