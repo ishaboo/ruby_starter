54ac595e8d1e24598a00aee0899e9a8fd1bec8d7
Pasquale Kress <wabisabisf@gmail.com>
2018-06-28 22:52:47 +0200

    initial monster controller commit

---
+++ mud/ideas/active_record/app/controllers/monsters_controller.rb
@@ -0,0 +1,15 @@
+class MonstersController
+  def initialize
+    @view = MonsterView.new
+  end
+
+  def fight
+    # a seperate/different fight method... call with engage, or perhaps have the monster initiate it
+  end
+
+  private
+
+  def locate_monster
+
+  end
+end
