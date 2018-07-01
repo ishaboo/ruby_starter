ded3902f092e49080555de0c68cc2761b3c48aee
Pasquale Kress <wabisabisf@gmail.com>
2018-07-01 20:25:11 +0200

    this method is still highly problematic as it only allows to go up to level 3 and it repeats redundant output

--- mud/ideas/active_record/app/models/level.rb
+++ mud/ideas/active_record/app/models/level.rb
@@ -4,10 +4,10 @@ class Level
   # level 3 --> 1101 - 1800 exp
 
   def level_up(char)
-    if char.exp > 500
+    if char.exp > 500 && char.level != 2 && char.level != 3
       char.level = 2
       puts "You leveled up"
-    elsif char.exp > 1100
+    elsif char.exp > 1100 && char.level != 3
       char.level = 3
       puts "Congrats, you are now at level 3"
     end
