49d42c18363d756a7896cd850793a37b2df385ce
Pasquale Kress <wabisabisf@gmail.com>
2018-07-14 23:44:13 +0200

    finding corpses and items works

--- mud/ideas/active_record/app/modules/modules.rb
+++ mud/ideas/active_record/app/modules/modules.rb
@@ -26,16 +26,22 @@ module Item
   end
 
   def Item.find_corpse(char)
-
+    bot = Bot.where(x_coord: char.x_coord, y_coord: char.y_coord)
+    unless bot.count == 0 || bot.first.alive == true
+      return bot.first
+    end
   end
 
   def Item.find_items(char)
-
+    stuff = []
+    corpse = Item.find_corpse(char)
+    stuff << items = corpse.inventory.inventory_items
+    stuff << weapons = corpse.inventory.weapons
   end
 
   # this method should only deal with adding the actual item to character model
-  def Item.get_item(char, items={})
-    items.each do |item, type|
+  def Item.get_item(char, inventory)
+    inventory.each do |item, type|
       if type == 'weapon'
         char.inventory.weapons << item
       elsif type == 'item'
