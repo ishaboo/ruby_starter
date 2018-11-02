6d6f4978ae34445c779b172b61386b00c2f140d3
wabi69sabi <wabisabisf@gmail.com>
2018-11-02 12:42:18 +0100

    added some monsters

--- mud/ideas/active_record/db/monster_seed.rb
+++ mud/ideas/active_record/db/monster_seed.rb
@@ -4,4 +4,5 @@ Monster.create(name: "Rat", description: "Rat's sometimes carry disease", hitpoi
 Monster.create(name: "Possum", description: "A large possum", hitpoints: 12, strength: 10, x_coord: 1, y_coord: -2, alive: true)
 Monster.create(name: "Roach", description: "Roaches are quick and some fly", hitpoints: 8, strength: 6, x_coord: 3, y_coord: 1, alive: true)
 Monster.create(name: "Dog", description: "A large dangerous looking dog", hitpoints: 27, strength: 3, x_coord: 1, y_coord: -1, alive: true)
-Monster.create(name: "Mouse", description: "Don't let the small size of the mouse fool you", hitpoints: 10, strength: 11, x_coord: 3, y_coord: 0, alive: true)
+Monster.create(name: "Mouse", description: "Don't let the small size of the mouse fool you", hitpoints: 9, strength: 11, x_coord: 3, y_coord: 0, alive: true)
+Monster.create(name: "Large Mouse", description: "This is a really large mouse", hitpoints: 11, strength: 12, x_coord: 2, y_coord: 0, alive: true)
