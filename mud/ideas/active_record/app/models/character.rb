14399fa9afda9cae61c8065640b99dd88f5d93c8
wabi69sabi <wabisabisf@gmail.com>
2018-11-08 15:05:08 +0100

    getting started on using disciplines

--- mud/ideas/active_record/app/models/character.rb
+++ mud/ideas/active_record/app/models/character.rb
@@ -4,8 +4,17 @@ class Character < ActiveRecord::Base
 
   disciplines = ["thief", "mage", "warrior"]
 
-  def has_discipline
+  def use_discipline
     # Internal method that determines special skills and abilities
+    case self.discipline
+    when 'thief'
+      # we need to add stealth or movement here to give an advantage to
+    when 'mage'
+      # need some kinda magic ability
+    when 'warrior'
+      # maybe a more fighting orientated skill would be better
+      self.strength += 2
+    end
   end
 
   def belongs_to_class
