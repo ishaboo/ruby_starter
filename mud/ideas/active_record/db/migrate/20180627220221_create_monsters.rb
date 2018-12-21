a591f2a2dfc908322ece0c5fae35a2ab7a484ff9
wabi69sabi <wabisabisf@gmail.com>
2018-12-21 14:57:40 +0100

    added default values to monster model

--- mud/ideas/active_record/db/migrate/20180627220221_create_monsters.rb
+++ mud/ideas/active_record/db/migrate/20180627220221_create_monsters.rb
@@ -5,9 +5,9 @@ class CreateMonsters < ActiveRecord::Migration[5.0]
       t.string :description
       t.integer :hitpoints
       t.integer :strength
-      t.integer :x_coord
-      t.integer :y_coord
-      t.boolean :alive
+      t.integer :x_coord, :default => 0
+      t.integer :y_coord, :default => 0
+      t.boolean :alive, :default => true
       t.timestamps
     end
   end
