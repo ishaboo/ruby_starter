358ac7bbf8466bcd2ce89ce20a748661538bb342
wabi69sabi <wabisabisf@gmail.com>
2018-12-29 21:18:55 +0100

    added attribute selection to character creation

--- mud/ideas/active_record/app/controllers/avatar_creation_controller.rb
+++ mud/ideas/active_record/app/controllers/avatar_creation_controller.rb
@@ -4,4 +4,33 @@ class AvatarCreationController
       Welcome to this awesome MUD.\n
       Before we can begin you will have to create a character.\n"
   end
+
+  def create_char
+    @char = Character.new
+  end
+
+  def give_attributes
+    puts "Give your character a name:\n> "
+    @char.name = Route.choice
+    puts "What race is your character [Elf, Dwarf, Human, Orc]:\n> "
+    @char.race = Route.choice
+    puts "Does your character have a discipline [thief, mage, warrior]?:\n> "
+    @char.discipline = Route.choice
+    @char.alive = true
+    @char.hitpoints = 25
+    @char.strength = 8
+    puts "This is what you picked so far:\n
+      #{@char.name}\n
+      #{@char.race}\n
+      #{@char.discipline}\n
+      #{@char.hitpoints}\n
+      #{@char.strength}\n
+      Do you want to save this character?"
+    ans = Route.choice
+    if ans == "yes"
+      @char.save
+    else
+      puts "You just wasted your time..."
+    end
+  end
 end
